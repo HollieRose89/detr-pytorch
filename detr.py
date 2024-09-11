b14cc9797aba17032bcaa78a756206797aadeae9
Hollie Rose <hollyrose0209@outlook.com>
2024-09-11 19:47:15 +0800

    Update detr.py

--- detr.py
+++ detr.py
@@ -25,7 +25,7 @@ class Detection_Transformers(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : '、content/detr-pytorch/model_data/detr_resnet50_weights_coco.pth',
+        "model_path"        : '/content/detr-pytorch/model_data/detr_resnet50_weights_coco.pth',
         "classes_path"      : '/content/detr-pytorch/model_data/coco_classes.txt',
         #---------------------------------------------------------------------#
         #   输入图片的大小
